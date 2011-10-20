cc96b412f9bf0983380861ddbce48278aebc3524
Tom JL Hope <tjlhope@gmail.com>
2011-10-20 14:38:46 +0100

    Add grep wrappers.

---
+++ wrappers/grep.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+[ -z ${1} -o "${1}" = '-h' ] && {
+    echo "Usage: $0 PATTERN [PATH[ PATH[ ...]]]"
+    exit
+}
+
+# prog specific options
+case "$(basename ${0})" in
+    lgrep)
+	sopts="-A 2 -B 2"
+	;;
+    rgrep)
+	sopts="-R"
+	;;
+esac
+
+# split opts and args
+while [ -n "${1}" ]
+do
+    case "${1}" in
+	-*e)
+	    [ "${1%e}" = '-' ] ||
+		opts="${opts}${1%e} "
+	    shift
+	    patts="${patts}-e ${1} "
+	    ;;
+	-*)
+	    opts="${opts}${1} "
+	    ;;
+	*)
+	    paths="${paths}${1} "
+	    ;;
+    esac
+    shift
+done
+
+# check for patt
+[ -z "${patts}" ] && {
+    patts="${paths%% *}"
+    paths="${paths#* }"
+}
+
+exec /bin/grep --color=auto ${sopts} ${opts} ${patts} ${paths:-./}
