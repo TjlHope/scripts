7b676ba0debdcf477d68301d05e3ed4f4d61bf62
Tom JL Hope <tjlhope@gmail.com>
2012-03-15 14:46:28 +0000

    Update emerge wrapper.

--- lib/wrappers/emerge.sh
+++ lib/wrappers/emerge.sh
@@ -5,31 +5,34 @@
 res=0
 exit_status=0
 
-case "$(/bin/basename $0)" in
+case "${0##*/}" in
 
     "emerge.update")
 
 	# fetch first ops
-	ops=""
-	while [ -n "$1" ]
+	opts=""
+	while [ -n "${1}" ]
 	do
-	    case "$1" in
+	    case "${1}" in
 		"--")
 		    shift
 		    break
 		    ;;
 		"-"*)
-		    ops="$ops $1"
+		    ops="${ops} ${1}"
 		    ;;
 		*)
-		    pkgs="$pkgs $1"
+		    pkgs="${pkgs} ${1}"
 		    ;;
 	    esac
 	    shift
 	done
 
-	### Update 
-	/usr/bin/emerge -auDN --keep-going ${pkgs:-@world} $ops	# update ops
+	### Update
+	/usr/bin/emerge --ask --update --deep --newuse --keep-going \
+	    --exclude "cross-*/*" ${opts} \
+	    ${pkgs:-@world}
+	# update ops
 	res=$? 
 	#echo "	Exit status: $res"
 
@@ -37,70 +40,73 @@ case "$(/bin/basename $0)" in
 	echo "$ops" | /bin/grep -e "-[^-	]*p" >/dev/null && exit $res
 	[ $res -ne 130 ] && [ $res -ne 102 ] || exit $res
 	#[ $res -eq 0 ] || exit $res
-	exit_status=$(( $exit_status + $res ))
+	exit_status=$(( ${exit_status} + ${res} ))
 
 	# find necesary module updates
-	update_mods="$(tac /var/log/emerge.log |
-	    sed -ne '0,/Started emerge on/s/.*>>> emerge.*\/\(python\|perl\|xorg-server\)-[0-9].*/\1/p')"
+	update_mods="$(tac /var/log/emerge.log | sed -ne \
+	    '0,/Started emerge on/ {
+		s/.*>>> emerge.*\/\(python\|perl\|xorg-server\)-[0-9].*/\1/p
+	    }')"
 
 	# fetch second ops
 	ops=""
-	while [ -n "$1" ]
+	while [ -n "${1}" ]
 	do
-	    if [ "$1" = "--" ]
+	    if [ "${1}" = "--" ]
 	    then
 		shift
 		break
 	    fi
-	    ops="$ops $1 "
+	    ops="${ops} ${1} "
 	    shift
 	done
 
 	### Remove uneccesary dependencies
-	[ $exit_status -eq 0 ] && /usr/bin/emerge --quiet --depclean $ops
-	res=$?							# depclean ops
+	[ ${exit_status} -eq 0 ] &&
+	    /usr/bin/emerge --quiet --depclean ${ops}		# depclean ops
+	res=${?}
 	#echo "	Exit status: $res"
 
 	# Catch errors and decide action
-	[ $res -ne 130 ] && [ $res -ne 102 ] || exit $res
+	[ ${res} -ne 130 ] && [ ${res} -ne 102 ] || exit ${res}
 	#[ $res -eq 0 ] || exit $res
-	exit_status=$(( $exit_status + $res ))
+	exit_status=$(( ${exit_status} + ${res} ))
 
 	### Rebuild packages with broken link level dependencies
-	/usr/bin/revdep-rebuild $@		# rest of them for rebuild
-	res=$? 
+	/usr/bin/revdep-rebuild ${@}		# rest of them for rebuild
+	res=${?}
 	#echo "	Exit status: $res"
 
 	# Catch errors and decide action
-	[ $res -ne 130 ] && [ $res -ne 102 ] || exit $res
+	[ ${res} -ne 130 ] && [ ${res} -ne 102 ] || exit ${res}
 	#[ $res -eq 0 ] || exit $res
-	exit_status=$(( $exit_status + $res ))
+	exit_status=$(( ${exit_status} + ${res} ))
 
 	# can't be bothered to try and parse ops for these as well
 
 	### Perform needed module updates
-	for x in "$update_mods"
+	for x in "${update_mods}"
 	do
-	    case "$x" in
+	    case "${x}" in
 		"python")
 		    /usr/bin/eselect python update --python2
 		    /usr/bin/eselect python update --python3
 		    /usr/sbin/python-updater
-		    res=$? 
+		    res=${?}
 		    ;;
 		"perl")
 		    /usr/sbin/perl-cleaner
-		    res=$? 
+		    res=${?}
 		    ;;
 		"xorg-server")
 		    /usr/bin/emerge -1 "$(qlist -qCU xorg-drivers | sed -e \
 			's:\([^ _]*\)_[^ _]*_\([^ _]*\):x11-drivers/xf86-\1-\2:g')"
-		    res=$?
+		    res=${?}
 		    ;;
 	    esac
 	    # Catch errors and decide action
-	    [ $res -ne 130 ] && [ $res -ne 102 ] || exit $res
-	    exit_status=$(( $exit_status + $res ))
+	    [ ${res} -ne 130 ] && [ ${res} -ne 102 ] || exit ${res}
+	    exit_status=$(( ${exit_status} + ${res} ))
 	done
 
 	### Clean uneeded tar.bz2's
@@ -126,14 +132,14 @@ case "$(/bin/basename $0)" in
 	#/usr/sbin/eupdatedb --quiet
 
 	eix-sync
-	exit_status=$?
+	exit_status=${?}
 	;;
 
     *)
-	echo "Invalid command: $0" >&2
+	echo "Invalid command: ${0}" >&2
 	exit 1
 	;;
 
 esac
 
-exit $exit_status
+exit ${exit_status}
